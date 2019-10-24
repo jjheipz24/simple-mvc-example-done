eab9acf8b9c7646fb2fe0df234537219b0159b95
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-10-24 13:14:52 -0400

    Dog functions made in controllers

--- server/models/Dog.js
+++ server/models/Dog.js
@@ -5,33 +5,38 @@ mongoose.Promise = global.Promise;
 let DogModel = {};
 
 const DogSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-    },
-    breed: {
-        type: String,
-        required: true,
-        trim: true,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  breed: {
+    type: String,
+    required: true,
+    trim: true,
 
-    },
-    age: {
-        type: Number,
-        min: 0,
-        required: true,
-    },
-    createdDate: {
-        type: Number,
-        default: Date.now
-    },
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  createdDate: {
+    type: Number,
+    default: Date.now,
+  },
 });
 
 DogSchema.statics.findByName = (name, callback) => {
-    const search = {
-        name,
-    };
+  const search = {
+    name,
+  };
 
-    return DogModel.findOne(search, callback);
-}
+  return DogModel.findOne(search, callback);
+};
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
